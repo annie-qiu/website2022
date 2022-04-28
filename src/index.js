ca094d2cdda6ca31cf8eb4f3a58468ef38d8b443
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-04-28 14:52:47 -0400

    bugg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 
-import App from './components/app';
+import App from './components/App';
 
 const store = configureStore({
   reducer: rootReducer,
