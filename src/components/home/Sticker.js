b8c2f8574e602b6b2626b024c21da3c621c6d204
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-07-25 13:49:07 -0400

    case change

---
+++ src/components/home/Sticker.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+import './home.scss';
+
+class Sticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: this.props.x,
+      y: this.props.y,
+    };
+  }
+
+  handleDrag = (e, ui) => {
+    const { x, y } = this.state;
+    this.setState({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+  };
+
+  render() {
+    return (
+      <Draggable
+        position={{
+          x: this.state.x, y: this.state.y,
+        }}
+        onDrag={this.handleDrag}
+        defaultPosition={{ x: this.props.x, y: this.props.y }}
+      >
+        <div className="sticker">
+          <img src={this.props.img} draggable="false" alt="img" style={{ width: this.props.width }} />
+        </div>
+      </Draggable>
+    );
+  }
+}
+
+export default Sticker;
---
+++ src/components/home/sticker_urls.js
@@ -0,0 +1,53 @@
+import bee from '../../assets/images/home_stickers/bee.png';
+import cow from '../../assets/images/home_stickers/cow.png';
+import flower from '../../assets/images/home_stickers/flower.png';
+import flower2 from '../../assets/images/home_stickers/flower2.png';
+import grain from '../../assets/images/home_stickers/grain.png';
+import mushroom from '../../assets/images/home_stickers/mushroom.png';
+import rabbit from '../../assets/images/home_stickers/rabbit.png';
+import tulip from '../../assets/images/home_stickers/tulip.png';
+
+const stickerUrls = [
+  {
+    img: bee,
+    x: 1200,
+    y: 340,
+  },
+  {
+    img: cow,
+    x: 906,
+    y: 450,
+  },
+  {
+    img: flower,
+    x: 300,
+    y: 600,
+  },
+  {
+    img: flower2,
+    x: 900,
+    y: 653,
+  },
+  {
+    img: grain,
+    x: 1163,
+    y: 509,
+  },
+  {
+    img: mushroom,
+    x: 180,
+    y: 557,
+  },
+  {
+    img: rabbit,
+    x: 400,
+    y: 511,
+  },
+  {
+    img: tulip,
+    x: 756,
+    y: 528,
+  },
+];
+
+export default stickerUrls;
